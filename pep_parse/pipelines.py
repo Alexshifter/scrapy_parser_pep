2209833c9f668a531f9c17dbcd901f2ddb0b9ee6
alexshifter <alexey1917@gmail.com>
2024-11-22 14:18:52 +0300

    fix_after_1st_rev

--- pep_parse/pipelines.py
+++ pep_parse/pipelines.py
@@ -1,34 +1,31 @@
 import csv
+from collections import defaultdict
 from datetime import datetime, timezone
 
-from pep_parse.settings import BASE_DIR
+from pep_parse.settings import BASE_DIR, RESULTS_DIR
 
 
 class PepParsePipeline:
+    def __init__(self):
+        self.results_dir = BASE_DIR / RESULTS_DIR
 
     def open_spider(self, spider):
-        self.status_summary = dict()
-        self.results_dir = BASE_DIR / 'results'
+        self.status_summary = defaultdict(int)
 
     def process_item(self, item, spider):
-        self.status_summary[item['status']] = (
-            self.status_summary.get(item['status'], 0) + 1)
+        self.status_summary[item['status']] += 1
         return item
 
     def close_spider(self, spider):
         cur_datetime = datetime.now(timezone.utc).strftime('%Y-%m-%d_%H-%M-%S')
         csv_file = self.results_dir / f'status_summary_{cur_datetime}.csv'
+        self.status_summary['Total'] = sum(self.status_summary.values())
+        rows = [
+            {'Статус': i, 'Количество': self.status_summary[i]}
+            for i in self.status_summary
+        ]
         with open(csv_file, 'w', newline='', encoding='utf-8') as csvfile:
             fieldnames = ['Статус', 'Количество']
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
             writer.writeheader()
-            for i in self.status_summary:
-                writer.writerow(
-                    {'Статус': i, 'Количество': self.status_summary[i]}
-                )
-            writer.writerow(
-                {
-                    'Статус': 'Total',
-                    'Количество': sum(self.status_summary.values())
-                }
-            )
+            writer.writerows(rows)
--- pep_parse/settings.py
+++ pep_parse/settings.py
@@ -2,10 +2,13 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+RESULTS_DIR = 'results'
+
 BOT_NAME = 'pep_parse'
 
-SPIDER_MODULES = ['pep_parse.spiders']
-NEWSPIDER_MODULE = 'pep_parse.spiders'
+NEWSPIDER_MODULE = f'{BOT_NAME}.spiders'
+
+SPIDER_MODULES = [f'{NEWSPIDER_MODULE}']
 
 ROBOTSTXT_OBEY = True
 
@@ -14,7 +17,7 @@ ITEM_PIPELINES = {
 }
 
 FEEDS = {
-    'results/pep_%(time)s.csv': {
+    RESULTS_DIR + '/pep_%(time)s.csv': {
         'format': 'csv',
         'fields': ['number', 'name', 'status'],
     },
